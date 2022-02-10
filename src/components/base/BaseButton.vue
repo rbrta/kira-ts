e6dc523829d6aa89774c36e1e17ad5179cb09386
= <=>
2022-02-10 18:59:21 +0500

    init

--- src/components/base/BaseButton.vue
+++ src/components/base/BaseButton.vue
@@ -1,13 +1,17 @@
 <template>
-  <div class="home">
-    <h1 class="text-3xl font-bold underline">Home</h1>
-  </div>
+  <button class="base-btn">
+    <slot></slot>
+  </button>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  name: "Home",
+  setup() {
+    return {};
+  },
 });
 </script>
+
+<style scoped></style>
