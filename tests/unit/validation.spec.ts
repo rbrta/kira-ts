e14c8599580b63ce3359fed53eee8976b4e74a64
= <=>
2022-02-21 10:30:46 +0500

    feat(validation): finish unit test

--- tests/unit/validation.spec.ts
+++ tests/unit/validation.spec.ts
@@ -1,17 +1,62 @@
 import useValidation from "@/composables/useValidation";
 import { ValidationFunction } from "@/composables/useValidation"
 
-test("password validation test", () => {
+describe("validation test", () => {
 	const passValidation: ValidationFunction = useValidation("password")
-	const wrongPasswords: string[]  = [
-		"",
-		" ",
-		"123",
-		"qwerty",
-		"QWERTY123",
-		"qwe123@",
-	]
-	wrongPasswords.forEach((el: string) => {
-		expect(passValidation(el)).toBe(false)
+	const emailValidation: ValidationFunction = useValidation("email")
+	const phoneValidation: ValidationFunction = useValidation("phone")
+
+	test("password invalid", () => {
+		const invalidPasswords: string[]  = [
+			"",
+			" ",
+			"123",
+			"123456789",
+			"qwerty",
+			"QWERTY123",
+			"qwe123@",
+			"!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+		]
+		invalidPasswords.forEach((el: string) => {
+			expect(passValidation(el)).toBe(false)
+		})
+	})
+
+	test("password valid", () => {
+		const validPasswords: string[] = [
+			"qwerty123!",
+			"@@@@@@@@@@@qwe3",
+			"12345678q!",
+			"q1!#$%&'()*+,-./:;<=>?@[\\]^_`{|}~"
+		]
+		validPasswords.forEach((el: string) => {
+			expect(passValidation(el)).toBe(true)
+		})
+	})
+
+	test("email invalid", () => {
+		const emailInvalid: string[] = [
+			"",
+			" ",
+			"a",
+			"a@",
+			"a@.co",
+			"a.com",
+			"a@@gmail.com",
+		]
+		emailInvalid.forEach((el: string) => {
+			expect(emailValidation(el)).toBe(false)
+		})
+	})
+
+	test("email valid", () => {
+		const emailValid: string[] = [
+			"a@a.co",
+			"aqwe1@gmail.com",
+			"_qw-ew@mail.ydsfdfdsf"
+		]
+		emailValid.forEach((el: string) => {
+			expect(emailValidation(el)).toBe(true)
+		})
 	})
 })
