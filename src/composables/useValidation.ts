5791ce9d24e3b6351ac2b0b0b70e88f6a1896658
= <=>
2022-02-21 15:41:38 +0500

    feat: finished migration to vite

--- src/composables/useValidation.ts
+++ src/composables/useValidation.ts
@@ -1,14 +1,3 @@
-export type ValidationFunction = (value: string) => boolean
-export default (type = "text"): ValidationFunction => {
-	switch (type) {
-		case "text": return textValidation
-		case "password": return passwordValidation
-		case "tel": return telValidation
-		case "email": return emailValidation
-		default: return textValidation
-	}
-}
-
 const emailRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 function emailValidation (value: string):boolean { return emailRegex.test(value.trim().toLowerCase())}
 
@@ -19,3 +8,14 @@ const telRegex = /^((8|\+7)[- ]?)?(\(?\d{3}\)?[- ]?)?[\d\- ]{10}$/
 function telValidation (value: string):boolean { return telRegex.test(value.trim()) }
 
 function textValidation (value: string):boolean { return value.split('').length > 0}
+
+export type ValidationFunction = (value: string) => boolean
+export default (type = "text"): ValidationFunction => {
+	switch (type) {
+		case "text": return textValidation
+		case "password": return passwordValidation
+		case "tel": return telValidation
+		case "email": return emailValidation
+		default: return textValidation
+	}
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,5 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from "vue-router";
-import Home from "../views/Home.vue";
+import Home from "@/views/Home.vue";
 
 const routes: Array<RouteRecordRaw> = [
 	{
@@ -10,21 +10,17 @@ const routes: Array<RouteRecordRaw> = [
 	{
 		path: "/about",
 		name: "About",
-		// route level code-splitting
-		// this generates a separate chunk (about.[hash].js) for this route
-		// which is lazy-loaded when the route is visited.
-		component: () =>
-			import(/* webpackChunkName: "about" */ "../views/About.vue"),
+		component: () => import("@/views/About.vue"),
 	},
 	{
 		path: "/kit",
 		name: "Kit",
-		component: () => import("../views/Kit.vue")
+		component: () => import("@/views/Kit.vue")
 	}
 ];
 
 const router = createRouter({
-	history: createWebHistory(process.env.BASE_URL),
+	history: createWebHistory(import.meta.env.BASE_URL),
 	routes,
 });
 
